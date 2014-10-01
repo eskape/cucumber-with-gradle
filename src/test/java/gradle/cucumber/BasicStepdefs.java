a24b7157f9f1f635e6088ccfb0ed1c99d9cd9814
Kishen Simbhoedatpanday <ksimbhoedatpanday@xebia.com>
2014-10-01 09:35:13 +0200

    Phone object created in main

--- src/test/java/gradle/cucumber/BasicStepdefs.java
+++ src/test/java/gradle/cucumber/BasicStepdefs.java
@@ -1,12 +1,13 @@
 package gradle.cucumber;
 
 import cucumber.api.java.en.When;
+import phonecatalog.Phone;
 
 public class BasicStepdefs {
 
 
     @When("^I run a step$")
     public void i_run_a_step() throws Throwable {
-        new Production().doWork();
+        new Phone().doWork();
     }
 }
